45151f40932d2f5da6fc4c1c3d3a706da139d363
Nikkirajput <0173cs221086@gmail.com>
2025-08-26 02:25:01 +0530

    fix static files config for Render

--- blogs/settings.py
+++ blogs/settings.py
@@ -138,4 +138,11 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional: whitenoise ke liye
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
