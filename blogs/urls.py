a21df9abf4a595e4520c4c369b555fb419c820d4
Nikkirajput <0173cs221086@gmail.com>
2025-08-26 16:40:50 +0530

    Initial commit

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,45 +1,19 @@
-"""
-URL configuration for blogs project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('', views.home,),
-    path('admin/', admin.site.urls),
-]
-
-urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
+    path('', views.home, name="home"),
     path('admin/', admin.site.urls),
-    path('', include('homeapp.urls')),
-    path('about/', include('aboutapp.urls')),
-    path('other/', include('otherapp.urls')),
-    path('portfolio/', include('portfolioapp.urls')),
-    path('blogs/', include('blogsapp.urls')),
+
+    # agar aapke apps hain to unhe bhi include karna hoga
+    # path('about/', include('aboutapp.urls')),
+    # path('blogs/', include('blogsapp.urls')),
 ]
 
-if settings.DEBUG:   # Development me hi serve karega
+# Development ke liye static + media serve karne ke liye
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
